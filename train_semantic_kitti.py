667c295219a89b11ef435b2ee063209591502147
Tiago Barros <tiago.barros.silva@hotmail.com>
2020-01-02 12:08:37 +0000

    fixes the log issue

--- train_semantic_kitti.py
+++ train_semantic_kitti.py
@@ -14,6 +14,8 @@ from torch.utils.data import DataLoader
 from torch.autograd import Variable
 import torch.optim as optim
 
+tf.compat.v1.disable_eager_execution()
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--epochs", type=int, default=300, help="number of epochs")
@@ -21,7 +23,7 @@ if __name__ == "__main__":
     parser.add_argument("--gradient_accumulations", type=int, default=2, help="number of gradient accums before step")
     parser.add_argument("--model_def", type=str, default="config/complex_yolov3.cfg", help="path to model definition file")
     parser.add_argument("--pretrained_weights", type=str, help="if specified starts from checkpoint model")
-    parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
+    parser.add_argument("--n_cpu", type=int, default=5, help="number of cpu threads to use during batch generation")
     parser.add_argument("--img_size", type=int, default=cnf.BEV_WIDTH, help="size of each image dimension")
     parser.add_argument("--evaluation_interval", type=int, default=2, help="interval evaluations on validation set")
     parser.add_argument("--multiscale_training", default=True, help="allow for multi-scale training")
