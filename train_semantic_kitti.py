6bc4a2e233171dc5c3e9210799d756f1ddd9a1e9
Tiago Barros <tiago.barros.silva@hotmail.com>
2020-01-09 14:16:14 +0000

    parameter ajustment

--- train_semantic_kitti.py
+++ train_semantic_kitti.py
@@ -19,13 +19,13 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser('-W ignore::UserWarning ./train_semantic_kitti.py')
     parser.add_argument("--epochs", type=int, default=300, help="number of epochs")
     parser.add_argument("--batch_size", type=int, default=3, help="size of each image batch")
-    parser.add_argument("--gradient_accumulations", type=int, default=2, help="number of gradient accums before step")
+    parser.add_argument("--gradient_accumulations", type=int, default=6, help="number of gradient accums before step")
     parser.add_argument("--model_def", type=str, default="config/complex_yolov3.cfg", help="path to model definition file")
-    parser.add_argument("--pretrained_weights", type=str, help="if specified starts from checkpoint model")
-    parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
+    parser.add_argument("--pretrained_weights", type=str)
+    parser.add_argument("--n_cpu", type=int, default=4, help="number of cpu threads to use during batch generation")
     parser.add_argument("--img_size", type=int, default=cnf.BEV_WIDTH, help="size of each image dimension")
-    parser.add_argument("--evaluation_interval", type=int, default=10, help="interval evaluations on validation set")
-    parser.add_argument("--multiscale_training", default=True, help="allow for multi-scale training")
+    parser.add_argument("--evaluation_interval", type=int, default=1, help="interval evaluations on validation set")
+    parser.add_argument("--multiscale_training", default=False, help="allow for multi-scale training")
     opt = parser.parse_args()
     print(opt)
 
@@ -93,7 +93,7 @@ if __name__ == "__main__":
 
         for batch_i, (_, imgs, targets) in enumerate(dataloader):
             batches_done = len(dataloader) * epoch + batch_i
-
+            #_, imgs, targets  = dataset.__getitem__(25)
             imgs = Variable(imgs.to(device))
             targets = Variable(targets.to(device), requires_grad=False)
 
