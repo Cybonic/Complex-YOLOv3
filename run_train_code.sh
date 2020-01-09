5b3f51e59f53b17e8ad469dc03eac444c680a7d0
Tiago Barros <tiago.barros.silva@hotmail.com>
2020-01-09 14:23:34 +0000

    shell comand update

--- run_train_code.sh
+++ run_train_code.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
 python -W ignore::UserWarning train_semantic_kitti.py --epochs 100 --batch_size 3
+
+python -W ignore::UserWarning train_semantic_kitti.py --epochs 600 --batch_size 3 --pretrained_weights checkpoints/yolov3_ckpt_epoch-8_MAP-0.03.pth
